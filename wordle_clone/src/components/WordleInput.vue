b4c3ecf6800a2e4f469ac4baa4140558a0c81fce
RyanLeber <ryanleber1@gmail.com>
2023-02-19 18:56:47 -0500

    removed "enter" key check for WordleInput

--- wordle_clone/src/components/WordleInput.vue
+++ wordle_clone/src/components/WordleInput.vue
@@ -1,85 +1,60 @@
 <!-- eslint-disable prettier/prettier -->
 <!-- eslint-disable prettier/prettier -->
 <script setup>
-import { reactive, ref, onMounted, onBeforeUnmount, defineComponent } from 'vue';
-
-const props = defineProps({
-  guess: String
+defineProps({
+  guess: Array
 })
-const temp = ref(props.guess)
-//const letterArray = reactive([]);
-//lettersArray.value = props.guess.split('')
 //const currentRow = ref(['row1', 'row2', 'row3', 'row4', 'row5', 'row6']);
-let n = ref(0);
-//let guesses = [];
-console.log('temp: ', temp)
-
-const nextRow = (event) =>{
-  if (event.key === "Enter" && n.value < 5) {
-    if(letterArray.value.length % 5 === 0) {
-      n.value++;
-      console.log(n)
-    }
-  }
-}
-
-onMounted(() => {
-  document.addEventListener('keydown', nextRow);
-})
-onBeforeUnmount(() => {
-  document.removeEventListener('keydown', nextRow);
-  n.value = 0;
-})
 </script>
 
 <template>
   <div class="answer-grid">
     <div class="answer-row" id="row1">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[0] }}</div>
+      <div class="answer-item">{{ guess[1] }}</div>
+      <div class="answer-item">{{ guess[2] }}</div>
+      <div class="answer-item">{{ guess[3] }}</div>
+      <div class="answer-item">{{ guess[4] }}</div>
     </div>
     <div class="answer-row" id="row2">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[5] }}</div>
+      <div class="answer-item">{{ guess[6] }}</div>
+      <div class="answer-item">{{ guess[7] }}</div>
+      <div class="answer-item">{{ guess[8] }}</div>
+      <div class="answer-item">{{ guess[9] }}</div>
     </div>
     <div class="answer-row" id="row3">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[10] }}</div>
+      <div class="answer-item">{{ guess[11] }}</div>
+      <div class="answer-item">{{ guess[12] }}</div>
+      <div class="answer-item">{{ guess[13] }}</div>
+      <div class="answer-item">{{ guess[14] }}</div>
     </div>
     <div class="answer-row" id="row4">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[15] }}</div>
+      <div class="answer-item">{{ guess[16] }}</div>
+      <div class="answer-item">{{ guess[17] }}</div>
+      <div class="answer-item">{{ guess[18] }}</div>
+      <div class="answer-item">{{ guess[19] }}</div>
     </div>
     <div class="answer-row" id="row5">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[20] }}</div>
+      <div class="answer-item">{{ guess[21] }}</div>
+      <div class="answer-item">{{ guess[22] }}</div>
+      <div class="answer-item">{{ guess[23] }}</div>
+      <div class="answer-item">{{ guess[24] }}</div>
     </div>
     <div class="answer-row" id="row6">
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
-      <div class="answer-item"></div>
+      <div class="answer-item">{{ guess[25] }}</div>
+      <div class="answer-item">{{ guess[26] }}</div>
+      <div class="answer-item">{{ guess[27] }}</div>
+      <div class="answer-item">{{ guess[28] }}</div>
+      <div class="answer-item">{{ guess[29] }}</div>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
   .answer-grid {
     padding: 10px;
@@ -106,6 +81,7 @@ onBeforeUnmount(() => {
     line-height: 1;
     font-family: var(--guess-font);
     font-weight: var(--guess-weight);
+    color: white;
     vertical-align: middle;
     box-sizing: border-box;
     text-transform: uppercase;
