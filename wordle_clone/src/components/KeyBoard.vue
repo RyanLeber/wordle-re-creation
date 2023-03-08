33d801e0da29154fe5f6cb91fd039e13ba063598
RyanLeber <ryanleber1@gmail.com>
2023-03-07 20:01:18 -0500

    Changed to buttons to call the getkey function

--- wordle_clone/src/components/KeyBoard.vue
+++ wordle_clone/src/components/KeyBoard.vue
@@ -1,46 +1,53 @@
 <!-- eslint-disable prettier/prettier -->
-<script>
+<script setup>
+import { ref } from 'vue'
 
+const selectedKey = ref('');
 
+function getKeyPress(event) {
+  selectedKey.value = event.target.getAttribute('data-key');
+  console.log(selectedKey.value)
+  emit()
+}
 </script>
 
 <template>
   <div class="keyboard-container">
     <div class="key-row">
-      <button type="button" data-key="q" class="key-key">q</button>
-      <button type="button" data-key="w" class="key-key">w</button>
-      <button type="button" data-key="e" class="key-key">e</button>
-      <button type="button" data-key="r" class="key-key">r</button>
-      <button type="button" data-key="t" class="key-key">t</button>
-      <button type="button" data-key="y" class="key-key">y</button>
-      <button type="button" data-key="u" class="key-key">u</button>
-      <button type="button" data-key="i" class="key-key">i</button>
-      <button type="button" data-key="o" class="key-key">o</button>
-      <button type="button" data-key="p" class="key-key">p</button>
+      <button @click="getKeyPress" data-key="q" class="key-key">q</button>
+      <button @click="getKeyPress" data-key="w" class="key-key">w</button>
+      <button @click="getKeyPress" data-key="e" class="key-key">e</button>
+      <button @click="getKeyPress" data-key="r" class="key-key">r</button>
+      <button @click="getKeyPress" data-key="t" class="key-key">t</button>
+      <button @click="getKeyPress" data-key="y" class="key-key">y</button>
+      <button @click="getKeyPress" data-key="u" class="key-key">u</button>
+      <button @click="getKeyPress" data-key="i" class="key-key">i</button>
+      <button @click="getKeyPress" data-key="o" class="key-key">o</button>
+      <button @click="getKeyPress" data-key="p" class="key-key">p</button>
     </div>
     <div class="key-row">
       <div data-spacer="spacer" class="key-spacer"></div>
-      <button type="button" data-key="a" class="key-key">a</button>
-      <button type="button" data-key="s" class="key-key">s</button>
-      <button type="button" data-key="d" class="key-key">d</button>
-      <button type="button" data-key="f" class="key-key">f</button>
-      <button type="button" data-key="g" class="key-key">g</button>
-      <button type="button" data-key="h" class="key-key">h</button>
-      <button type="button" data-key="j" class="key-key">j</button>
-      <button type="button" data-key="k" class="key-key">k</button>
-      <button type="button" data-key="l" class="key-key">l</button>
+      <button @click="getKeyPress" data-key="a" class="key-key">a</button>
+      <button @click="getKeyPress" data-key="s" class="key-key">s</button>
+      <button @click="getKeyPress" data-key="d" class="key-key">d</button>
+      <button @click="getKeyPress" data-key="f" class="key-key">f</button>
+      <button @click="getKeyPress" data-key="g" class="key-key">g</button>
+      <button @click="getKeyPress" data-key="h" class="key-key">h</button>
+      <button @click="getKeyPress" data-key="j" class="key-key">j</button>
+      <button @click="getKeyPress" data-key="k" class="key-key">k</button>
+      <button @click="getKeyPress" data-key="l" class="key-key">l</button>
       <div data-spacer="spacer" class="key-spacer"></div>
     </div>
     <div class="key-row">
-      <button type="button" data-key="enter" class="key-key key-enter">enter</button>
-      <button type="button" data-key="x" class="key-key">z</button>
-      <button type="button" data-key="c" class="key-key">x</button>
-      <button type="button" data-key="v" class="key-key">c</button>
-      <button type="button" data-key="b" class="key-key">v</button>
-      <button type="button" data-key="n" class="key-key">b</button>
-      <button type="button" data-key="m" class="key-key">n</button>
-      <button type="button" data-key="m" class="key-key">m</button>
-      <button type="button" data-key="backspace" class="key-key key-remove">del</button>
+      <button @click="getKeyPress" data-key="enter" class="key-key key-enter">enter</button>
+      <button @click="getKeyPress" data-key="x" class="key-key">z</button>
+      <button @click="getKeyPress" data-key="c" class="key-key">x</button>
+      <button @click="getKeyPress" data-key="v" class="key-key">c</button>
+      <button @click="getKeyPress" data-key="b" class="key-key">v</button>
+      <button @click="getKeyPress" data-key="n" class="key-key">b</button>
+      <button @click="getKeyPress" data-key="m" class="key-key">n</button>
+      <button @click="getKeyPress" data-key="m" class="key-key">m</button>
+      <button @click="getKeyPress" data-key="backspace" class="key-key key-remove">del</button>
     </div>
   </div>
 </template>
