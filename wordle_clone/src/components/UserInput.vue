d9b3cba74fec4dddaf74e0a9ab16e63759857226
RyanLeber <ryanleber1@gmail.com>
2023-03-07 19:23:57 -0500

    Moved api request to CheckAnswer

--- wordle_clone/src/components/UserInput.vue
+++ wordle_clone/src/components/UserInput.vue
@@ -1,12 +1,16 @@
 <!-- eslint-disable prettier/prettier -->
 <script setup>
+import WordleInput from "../components/WordleInput.vue";
+import KeyBoard from "../components/KeyBoard.vue";
+import CheckAnswer from "../components/CheckAnswer.vue";
 import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 // change to reactive
 const letters = reactive({arr: []});
-const emit = defineEmits(['response']);
+//const emit = defineEmits(['response']);
 const n = ref(0);
 const lettersAmount = ref(5);
-const answer = reactive({answer: ''});
+
+
 
 const handleKeydown = (event) => {
   // New letter method
@@ -26,34 +30,14 @@ const handleKeydown = (event) => {
       for (let i = letters.arr.length - 5; i < lettersAmount.value; i++){
         check += letters.arr[i];
       }
-      if (answer.answer.word === check) {
-        console.log('correct!')
-      }
+      console.log(check)
       n.value++;
       lettersAmount.value += 5;
     }
   }
-  emit('response', letters.arr);
-}
-
-async function getWord() {
-  const options = {
-    method: 'GET',
-	  headers: {
-      'X-RapidAPI-Key': '14ef35e71fmsh23f5263afc6e55ep1e1633jsn9c3fbae78461',
-      'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-	  }
-  }
-  const res = await fetch(
-    'https://wordsapiv1.p.rapidapi.com/words/?random=true&letters=5',
-    options
-  );
-  answer.answer = await res.json();
-  console.log(answer.answer.word)
 }
 
 onMounted(() => {
-  getWord();
   document.addEventListener('keydown', handleKeydown);
 })
 onBeforeUnmount(() => {
@@ -61,3 +45,26 @@ onBeforeUnmount(() => {
   n.value = 0;
 })
 </script>
+
+<template>
+  <div class="input-container">
+    <WordleInput :guess="letters.arr" />
+  </div>
+  <div class="keyboard-container">
+    <KeyBoard />
+  </div>
+  <div>
+    <CheckAnswer />
+  </div>
+</template>
+
+<style>
+.input-container {
+  display: flex;
+  padding: 5%;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  overflow: hidden;
+}
+</style>
