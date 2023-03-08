03a166c92f1b197062f74c428e3dda9712bf0a3e
RyanLeber <ryanleber1@gmail.com>
2023-03-07 19:21:13 -0500

    Added component to check guess

---
+++ wordle_clone/src/components/CheckAnswer.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable prettier/prettier -->
+<script setup>
+import { reactive } from 'vue';
+
+const answer = reactive({answer: ''});
+
+async function getWord() {
+  const options = {
+    method: 'GET',
+	  headers: {
+      'X-RapidAPI-Key': '14ef35e71fmsh23f5263afc6e55ep1e1633jsn9c3fbae78461',
+      'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	  }
+  }
+  const res = await fetch(
+    'https://wordsapiv1.p.rapidapi.com/words/?random=true&letters=5',
+    options
+  );
+  answer.answer = await res.json();
+  console.log(answer.answer.word)
+}
+</script>
+
+<template>
+  <button @click="getWord">New Word</button>
+</template>
